903df56f47bcd92fc07515c732eecac7d4713093
Sri hari Sreenivasan <122223722+Srihari0911@users.noreply.github.com>
2023-08-04 21:53:38 -0400

    Add files via upload

---
+++ BookDatabase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookDatabase.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
