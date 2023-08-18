581f338d96306cfe1b584f15d3c50eab7ecc47f3
Sri hari Sreenivasan <122223722+Srihari0911@users.noreply.github.com>
2023-08-18 19:30:10 -0400

    Create SellerMaster.Master.designer.cs

---
+++ BookDatabase/Views/Seller/SellerMaster.Master.designer.cs
@@ -0,0 +1,34 @@
+
+namespace BookDatabase.Views.Seller {
+    
+    
+    public partial class SellerMaster {
+        
+        /// <summary>
+        /// head control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// MyContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MyContent;
+    }
+}
